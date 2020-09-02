237dd9f8dab87aa6a75a30e9e30dfe14e840a671
thiagogre <thiagoluiz_16@hotmail.com>
2020-09-02 18:49:17 -0300

    feat: create a function to convert data to better print format

---
+++ convert_print_format.py
@@ -0,0 +1,38 @@
+from openpyxl import load_workbook
+
+
+def formating():
+    path = 'C:/Users/thiag/Desktop/Atlas/test.xlsx'
+
+    loading = load_workbook(filename=path)
+    la = loading.active
+    la.delete_cols(5)
+    la.delete_cols(6)
+    la.delete_cols(7)
+    la.delete_cols(7)
+    la.delete_cols(8, 11)
+
+    la.move_range('E1:E100', rows=0, cols=3)
+    la.move_range('G1:G100', rows=0, cols=-2)
+    la.move_range('H1:H100', rows=0, cols=-1)
+
+    la['H1'] = 'Tipo de NF'
+    la['I1'] = 'Informar NF gerada'
+
+    for i in range(2, 100, 1):
+        if (i % 2) != 0:
+            textoCodeMedida = 'E'+str(i)
+            la.move_range(textoCodeMedida, rows=-1, cols=2)
+            textoMedidas = 'G'+str(i)
+            la.move_range(textoMedidas, rows=-1, cols=1)
+        else:
+            textoMedidas = 'G'+str(i)
+            la.move_range(textoMedidas, rows=0, cols=-1)
+
+    for j in range(3, 100, 1):
+        la.delete_rows(j)
+
+    loading.save(path)
+
+
+formating()
