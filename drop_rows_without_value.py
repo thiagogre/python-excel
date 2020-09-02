158c672eaa933dce1a50cdcf4b98c27a474b56ee
thiagogre <thiagoluiz_16@hotmail.com>
2020-09-02 19:01:38 -0300

    feat: create a new code for drop rows without and repeated values

---
+++ drop_rows_without_value.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from openpyxl import load_workbook
+
+excel_file = 'C:/Users/thiag/Desktop/Atlas/QMtest.xlsx'
+
+loading = load_workbook(filename=excel_file)
+la = loading.active
+la.delete_cols(7)
+la.delete_cols(7)
+la.delete_cols(7)
+la.delete_cols(7)
+la.delete_cols(8)
+la.delete_cols(8)
+la.delete_cols(8)
+la.delete_cols(8)
+la.delete_cols(5)
+loading.save(excel_file)
+
+df1 = pd.read_excel(excel_file, 'Sheet1')
+df2 = pd.DataFrame(df1)
+empDfObj = pd.DataFrame(df1)
+modDf = empDfObj.dropna()
+byNotaDf = (modDf.set_index(['Nota', 'Material', 'Iníc.planj', 'Texto breve material','Texto das medidas','Texto de code medida']))
+byNotaDf.to_excel(excel_file)
