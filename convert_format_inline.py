34f85683ca7fdde32001e8a8441cbbf685e07e1e
thiagogre <thiagoluiz_16@hotmail.com>
2020-09-06 08:33:03 -0300

    refactor: changed variable names

--- convert_format_inline.py
+++ convert_format_inline.py
@@ -18,204 +18,215 @@ la.delete_cols(8)
 la.delete_cols(5)
 loading.save(excel_file_inline)
 
-
 dfInline = pd.read_excel(excel_file_inline)
-loading1 = load_workbook(filename=excel_file_inline)
-la1 = loading1.active
-j = 2
+loading_1 = load_workbook(filename=excel_file_inline)
+la_1 = loading_1.active
+compensating_value = 2
 repeated_notes = {}
 copy = ''
 aux = 0
-for index in dfInline['Nota']:
 
-    repeated_notes[index] = Counter(dfInline['Nota'])[index]
+for note in dfInline['Nota']:
 
-    if (repeated_notes[index] == 2) and (copy != index):
-        textoDasMedidas = 'E'+str(j)
-        la1.move_range(textoDasMedidas, rows=1, cols=2)
-        textoCodeMedidas = 'F'+str(j)
-        la1.move_range(textoCodeMedidas, rows=1, cols=2)
-        copy = index
+    repeated_notes[note] = Counter(dfInline['Nota'])[note]
 
-    if (repeated_notes[index] == 3) and (copy != index):
+    if (repeated_notes[note] == 2) and (copy != note):
+        measure_text = 'E'+str(compensating_value)
+        la_1.move_range(measure_text, rows=1, cols=2)
+        measure_code_text = 'F'+str(compensating_value)
+        la_1.move_range(measure_code_text, rows=1, cols=2)
+        copy = note
+
+    if (repeated_notes[note] == 3) and (copy != note):
         if aux == 0:
-            textoDasMedidas = 'E'+str(j+1)
-            la1.move_range(textoDasMedidas, rows=-1, cols=2)
-            textoCodeMedidas = 'F'+str(j+1)
-            la1.move_range(textoCodeMedidas, rows=-1, cols=3)
+            measure_text = 'E'+str(compensating_value+1)
+            la_1.move_range(measure_text, rows=-1, cols=2)
+            measure_code_text = 'F'+str(compensating_value+1)
+            la_1.move_range(measure_code_text, rows=-1, cols=3)
             aux += 1
-            copy = index
+            copy = note
             if aux == 1:
-                textoDasMedidas = 'E'+str(j+2)
-                la1.move_range(textoDasMedidas, rows=-2, cols=3)
-                textoCodeMedidas = 'F'+str(j+2)
-                la1.move_range(textoCodeMedidas, rows=-2, cols=4)
+                measure_text = 'E'+str(compensating_value+2)
+                la_1.move_range(measure_text, rows=-2, cols=3)
+                measure_code_text = 'F'+str(compensating_value+2)
+                la_1.move_range(measure_code_text, rows=-2, cols=4)
                 aux = 0
 
-    if (repeated_notes[index] == 4) and (copy != index):
+    if (repeated_notes[note] == 4) and (copy != note):
         if aux == 0:
-            textoDasMedidas = 'E'+str(j+1)
-            la1.move_range(textoDasMedidas, rows=-1, cols=2)
-            textoCodeMedidas = 'F'+str(j+1)
-            la1.move_range(textoCodeMedidas, rows=-1, cols=2)
+            measure_text = 'E'+str(compensating_value+1)
+            la_1.move_range(measure_text, rows=-1, cols=2)
+            measure_code_text = 'F'+str(compensating_value+1)
+            la_1.move_range(measure_code_text, rows=-1, cols=2)
             aux += 1
-            copy = index
+            copy = note
             if aux == 1:
-                textoDasMedidas = 'E'+str(j+2)
-                la1.move_range(textoDasMedidas, rows=-2, cols=4)
-                textoCodeMedidas = 'F'+str(j+2)
-                la1.move_range(textoCodeMedidas, rows=-2, cols=4)
+                measure_text = 'E'+str(compensating_value+2)
+                la_1.move_range(measure_text, rows=-2, cols=4)
+                measure_code_text = 'F'+str(compensating_value+2)
+                la_1.move_range(measure_code_text, rows=-2, cols=4)
                 aux += 1
                 if aux == 2:
-                    textoDasMedidas = 'E'+str(j+3)
-                    la1.move_range(textoDasMedidas, rows=-3, cols=6)
-                    textoCodeMedidas = 'F'+str(j+3)
-                    la1.move_range(textoCodeMedidas, rows=-3, cols=6)
+                    measure_text = 'E'+str(compensating_value+3)
+                    la_1.move_range(measure_text, rows=-3, cols=6)
+                    measure_code_text = 'F'+str(compensating_value+3)
+                    la_1.move_range(measure_code_text, rows=-3, cols=6)
                     aux = 0
 
-    if (repeated_notes[index] == 5) and (copy != index):
+    if (repeated_notes[note] == 5) and (copy != note):
         if aux == 0:
-            textoDasMedidas = 'E'+str(j+1)
-            la1.move_range(textoDasMedidas, rows=-1, cols=2)
-            textoCodeMedidas = 'F'+str(j+1)
-            la1.move_range(textoCodeMedidas, rows=-1, cols=2)
+            measure_text = 'E'+str(compensating_value+1)
+            la_1.move_range(measure_text, rows=-1, cols=2)
+            measure_code_text = 'F'+str(compensating_value+1)
+            la_1.move_range(measure_code_text, rows=-1, cols=2)
             aux += 1
-            copy = index
+            copy = note
             if aux == 1:
-                textoDasMedidas = 'E'+str(j+2)
-                la1.move_range(textoDasMedidas, rows=-2, cols=4)
-                textoCodeMedidas = 'F'+str(j+2)
-                la1.move_range(textoCodeMedidas, rows=-2, cols=4)
+                measure_text = 'E'+str(compensating_value+2)
+                la_1.move_range(measure_text, rows=-2, cols=4)
+                measure_code_text = 'F'+str(compensating_value+2)
+                la_1.move_range(measure_code_text, rows=-2, cols=4)
                 aux += 1
                 if aux == 2:
-                    textoDasMedidas = 'E'+str(j+3)
-                    la1.move_range(textoDasMedidas, rows=-3, cols=6)
-                    textoCodeMedidas = 'F'+str(j+3)
-                    la1.move_range(textoCodeMedidas, rows=-3, cols=6)
+                    measure_text = 'E'+str(compensating_value+3)
+                    la_1.move_range(measure_text, rows=-3, cols=6)
+                    measure_code_text = 'F'+str(compensating_value+3)
+                    la_1.move_range(measure_code_text, rows=-3, cols=6)
                     aux += 1
                     if aux == 3:
-                        textoDasMedidas = 'E'+str(j+4)
-                        la1.move_range(textoDasMedidas, rows=-4, cols=8)
-                        textoCodeMedidas = 'F'+str(j+4)
-                        la1.move_range(textoCodeMedidas, rows=-4, cols=8)
+                        measure_text = 'E'+str(compensating_value+4)
+                        la_1.move_range(measure_text, rows=-4, cols=8)
+                        measure_code_text = 'F'+str(compensating_value+4)
+                        la_1.move_range(measure_code_text, rows=-4, cols=8)
                         aux = 0
-    if (repeated_notes[index] == 6) and (copy != index):
+    if (repeated_notes[note] == 6) and (copy != note):
         if aux == 0:
-            textoDasMedidas = 'E'+str(j+1)
-            la1.move_range(textoDasMedidas, rows=-1, cols=2)
-            textoCodeMedidas = 'F'+str(j+1)
-            la1.move_range(textoCodeMedidas, rows=-1, cols=2)
+            measure_text = 'E'+str(compensating_value+1)
+            la_1.move_range(measure_text, rows=-1, cols=2)
+            measure_code_text = 'F'+str(compensating_value+1)
+            la_1.move_range(measure_code_text, rows=-1, cols=2)
             aux += 1
-            copy = index
+            copy = note
             if aux == 1:
-                textoDasMedidas = 'E'+str(j+2)
-                la1.move_range(textoDasMedidas, rows=-2, cols=4)
-                textoCodeMedidas = 'F'+str(j+2)
-                la1.move_range(textoCodeMedidas, rows=-2, cols=4)
+                measure_text = 'E'+str(compensating_value+2)
+                la_1.move_range(measure_text, rows=-2, cols=4)
+                measure_code_text = 'F'+str(compensating_value+2)
+                la_1.move_range(measure_code_text, rows=-2, cols=4)
                 aux += 1
                 if aux == 2:
-                    textoDasMedidas = 'E'+str(j+3)
-                    la1.move_range(textoDasMedidas, rows=-3, cols=6)
-                    textoCodeMedidas = 'F'+str(j+3)
-                    la1.move_range(textoCodeMedidas, rows=-3, cols=6)
+                    measure_text = 'E'+str(compensating_value+3)
+                    la_1.move_range(measure_text, rows=-3, cols=6)
+                    measure_code_text = 'F'+str(compensating_value+3)
+                    la_1.move_range(measure_code_text, rows=-3, cols=6)
                     aux += 1
                     if aux == 3:
-                        textoDasMedidas = 'E'+str(j+4)
-                        la1.move_range(textoDasMedidas, rows=-4, cols=8)
-                        textoCodeMedidas = 'F'+str(j+4)
-                        la1.move_range(textoCodeMedidas, rows=-4, cols=8)
+                        measure_text = 'E'+str(compensating_value+4)
+                        la_1.move_range(measure_text, rows=-4, cols=8)
+                        measure_code_text = 'F'+str(compensating_value+4)
+                        la_1.move_range(measure_code_text, rows=-4, cols=8)
                         aux += 1
                         if aux == 4:
-                            textoDasMedidas = 'E'+str(j+5)
-                            la1.move_range(textoDasMedidas, rows=-5, cols=10)
-                            textoCodeMedidas = 'F'+str(j+5)
-                            la1.move_range(textoCodeMedidas, rows=-5, cols=10)
+                            measure_text = 'E'+str(compensating_value+5)
+                            la_1.move_range(measure_text, rows=-5, cols=10)
+                            measure_code_text = 'F'+str(compensating_value+5)
+                            la_1.move_range(
+                                measure_code_text, rows=-5, cols=10)
                             aux = 0
-    if (repeated_notes[index] == 7) and (copy != index):
+    if (repeated_notes[note] == 7) and (copy != note):
         if aux == 0:
-            textoDasMedidas = 'E'+str(j+1)
-            la1.move_range(textoDasMedidas, rows=-1, cols=2)
-            textoCodeMedidas = 'F'+str(j+1)
-            la1.move_range(textoCodeMedidas, rows=-1, cols=2)
+            measure_text = 'E'+str(compensating_value+1)
+            la_1.move_range(measure_text, rows=-1, cols=2)
+            measure_code_text = 'F'+str(compensating_value+1)
+            la_1.move_range(measure_code_text, rows=-1, cols=2)
             aux += 1
-            copy = index
+            copy = note
             if aux == 1:
-                textoDasMedidas = 'E'+str(j+2)
-                la1.move_range(textoDasMedidas, rows=-2, cols=4)
-                textoCodeMedidas = 'F'+str(j+2)
-                la1.move_range(textoCodeMedidas, rows=-2, cols=4)
+                measure_text = 'E'+str(compensating_value+2)
+                la_1.move_range(measure_text, rows=-2, cols=4)
+                measure_code_text = 'F'+str(compensating_value+2)
+                la_1.move_range(measure_code_text, rows=-2, cols=4)
                 aux += 1
                 if aux == 2:
-                    textoDasMedidas = 'E'+str(j+3)
-                    la1.move_range(textoDasMedidas, rows=-3, cols=6)
-                    textoCodeMedidas = 'F'+str(j+3)
-                    la1.move_range(textoCodeMedidas, rows=-3, cols=6)
+                    measure_text = 'E'+str(compensating_value+3)
+                    la_1.move_range(measure_text, rows=-3, cols=6)
+                    measure_code_text = 'F'+str(compensating_value+3)
+                    la_1.move_range(measure_code_text, rows=-3, cols=6)
                     aux += 1
                     if aux == 3:
-                        textoDasMedidas = 'E'+str(j+4)
-                        la1.move_range(textoDasMedidas, rows=-4, cols=8)
-                        textoCodeMedidas = 'F'+str(j+4)
-                        la1.move_range(textoCodeMedidas, rows=-4, cols=8)
+                        measure_text = 'E'+str(compensating_value+4)
+                        la_1.move_range(measure_text, rows=-4, cols=8)
+                        measure_code_text = 'F'+str(compensating_value+4)
+                        la_1.move_range(measure_code_text, rows=-4, cols=8)
                         aux += 1
                         if aux == 4:
-                            textoDasMedidas = 'E'+str(j+5)
-                            la1.move_range(textoDasMedidas, rows=-5, cols=10)
-                            textoCodeMedidas = 'F'+str(j+5)
-                            la1.move_range(textoCodeMedidas, rows=-5, cols=10)
+                            measure_text = 'E'+str(compensating_value+5)
+                            la_1.move_range(measure_text, rows=-5, cols=10)
+                            measure_code_text = 'F'+str(compensating_value+5)
+                            la_1.move_range(
+                                measure_code_text, rows=-5, cols=10)
                             aux += 1
                             if aux == 5:
-                                textoDasMedidas = 'E'+str(j+6)
-                                la1.move_range(textoDasMedidas, rows=-6, cols=12)
-                                textoCodeMedidas = 'F'+str(j+6)
-                                la1.move_range(textoCodeMedidas, rows=-6, cols=12)
+                                measure_text = 'E'+str(compensating_value+6)
+                                la_1.move_range(measure_text, rows=-6, cols=12)
+                                measure_code_text = 'F'+str(
+                                    compensating_value+6)
+                                la_1.move_range(
+                                    measure_code_text, rows=-6, cols=12)
                                 aux = 0
-    if (repeated_notes[index] == 8) and (copy != index):
+    if (repeated_notes[note] == 8) and (copy != note):
         if aux == 0:
-            textoDasMedidas = 'E'+str(j+1)
-            la1.move_range(textoDasMedidas, rows=-1, cols=2)
-            textoCodeMedidas = 'F'+str(j+1)
-            la1.move_range(textoCodeMedidas, rows=-1, cols=2)
+            measure_text = 'E'+str(compensating_value+1)
+            la_1.move_range(measure_text, rows=-1, cols=2)
+            measure_code_text = 'F'+str(compensating_value+1)
+            la_1.move_range(measure_code_text, rows=-1, cols=2)
             aux += 1
-            copy = index
+            copy = note
             if aux == 1:
-                textoDasMedidas = 'E'+str(j+2)
-                la1.move_range(textoDasMedidas, rows=-2, cols=4)
-                textoCodeMedidas = 'F'+str(j+2)
-                la1.move_range(textoCodeMedidas, rows=-2, cols=4)
+                measure_text = 'E'+str(compensating_value+2)
+                la_1.move_range(measure_text, rows=-2, cols=4)
+                measure_code_text = 'F'+str(compensating_value+2)
+                la_1.move_range(measure_code_text, rows=-2, cols=4)
                 aux += 1
                 if aux == 2:
-                    textoDasMedidas = 'E'+str(j+3)
-                    la1.move_range(textoDasMedidas, rows=-3, cols=6)
-                    textoCodeMedidas = 'F'+str(j+3)
-                    la1.move_range(textoCodeMedidas, rows=-3, cols=6)
+                    measure_text = 'E'+str(compensating_value+3)
+                    la_1.move_range(measure_text, rows=-3, cols=6)
+                    measure_code_text = 'F'+str(compensating_value+3)
+                    la_1.move_range(measure_code_text, rows=-3, cols=6)
                     aux += 1
                     if aux == 3:
-                        textoDasMedidas = 'E'+str(j+4)
-                        la1.move_range(textoDasMedidas, rows=-4, cols=8)
-                        textoCodeMedidas = 'F'+str(j+4)
-                        la1.move_range(textoCodeMedidas, rows=-4, cols=8)
+                        measure_text = 'E'+str(compensating_value+4)
+                        la_1.move_range(measure_text, rows=-4, cols=8)
+                        measure_code_text = 'F'+str(compensating_value+4)
+                        la_1.move_range(measure_code_text, rows=-4, cols=8)
                         aux += 1
                         if aux == 4:
-                            textoDasMedidas = 'E'+str(j+5)
-                            la1.move_range(textoDasMedidas, rows=-5, cols=10)
-                            textoCodeMedidas = 'F'+str(j+5)
-                            la1.move_range(textoCodeMedidas, rows=-5, cols=10)
+                            measure_text = 'E'+str(compensating_value+5)
+                            la_1.move_range(measure_text, rows=-5, cols=10)
+                            measure_code_text = 'F'+str(compensating_value+5)
+                            la_1.move_range(
+                                measure_code_text, rows=-5, cols=10)
                             aux += 1
                             if aux == 5:
-                                textoDasMedidas = 'E'+str(j+6)
-                                la1.move_range(textoDasMedidas, rows=-6, cols=12)
-                                textoCodeMedidas = 'F'+str(j+6)
-                                la1.move_range(textoCodeMedidas, rows=-6, cols=12)
+                                measure_text = 'E'+str(compensating_value+6)
+                                la_1.move_range(measure_text, rows=-6, cols=12)
+                                measure_code_text = 'F'+str(
+                                    compensating_value+6)
+                                la_1.move_range(
+                                    measure_code_text, rows=-6, cols=12)
                                 aux += 1
                                 if aux == 6:
-                                    textoDasMedidas = 'E'+str(j+7)
-                                    la1.move_range(textoDasMedidas, rows=-7, cols=14)
-                                    textoCodeMedidas = 'F'+str(j+7)
-                                    la1.move_range(textoCodeMedidas, rows=-7, cols=14)
+                                    measure_text = 'E'+str(
+                                        compensating_value+7)
+                                    la_1.move_range(
+                                        measure_text, rows=-7, cols=14)
+                                    measure_code_text = 'F'+str(
+                                        compensating_value+7)
+                                    la_1.move_range(
+                                        measure_code_text, rows=-7, cols=14)
                                     aux = 0
-    j += 1
+    compensating_value += 1
 
-loading1.save(excel_file_inline)
+loading_1.save(excel_file_inline)
 df = pd.read_excel(excel_file_inline)
 df['Texto das medidas'].replace('', np.nan, inplace=True)
 df.dropna(subset=['Texto das medidas'], inplace=True)
@@ -237,4 +248,3 @@ df.rename(columns={
 }, inplace=True)
 
 df.to_excel(excel_file_inline)
-
